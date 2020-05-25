591e81123a12f227f64d80120fbb7807eaf9690d
Vanderluizsj <48038130+Vanderluizsj@users.noreply.github.com>
2020-05-25 00:37:26 -0300

    aula2 e 3

---
+++ aula2.js
@@ -0,0 +1,6 @@
+var app=new Vue({   //instância que está sendo renderizada ao executar o html
+    el:'#app', //el indica que local sera acessado, no caso esta sndo usado um seletor de css, mas pode ser uma tag html
+    data:{
+        message:'Olá, mundo!'
+    }
+});
